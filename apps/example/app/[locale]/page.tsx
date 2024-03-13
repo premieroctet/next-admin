5e3c05f69afeaa7d247138de3a13087bcdc88f8c
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2024-03-13 10:29:24 +0100

    chore(example): redirect homepage to admin dashboard (#170)

--- apps/example/app/[locale]/page.tsx
+++ apps/example/app/[locale]/page.tsx
@@ -1,28 +1,5 @@
-import Link from "next/link";
-import { Card, Flex, Text, Title } from "@tremor/react";
+import { redirect } from "next/navigation";
 
 export default function Web() {
-  return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      className="h-screen bg-slate-100"
-    >
-      <Card className="max-w-md">
-        <Title className="text-2xl font-bold mb-4 text-center">
-          Next Admin
-        </Title>
-        <Text className="text-center mb-6">
-          Unleash your potential. Empower users. Simplify development. Get
-          started now!
-        </Text>
-        <Link
-          href="/admin"
-          className="block mx-auto px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition duration-300 text-center"
-        >
-          <Text className=" text-inherit">Explore Next Admin</Text>
-        </Link>
-      </Card>
-    </Flex>
-  );
+  redirect("/admin");
 }
