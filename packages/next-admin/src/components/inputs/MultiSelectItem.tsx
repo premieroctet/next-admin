41a0c22749b491bc9deffce6acdd28e46c23d404
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2024-04-16 16:56:33 +0200

    feat: add possibility to disable fields

--- packages/next-admin/src/components/inputs/MultiSelectItem.tsx
+++ packages/next-admin/src/components/inputs/MultiSelectItem.tsx
@@ -1,16 +1,24 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-const MultiSelectItem = ({ label, onRemoveClick }: any) => {
+type Props = {
+  label: string;
+  onRemoveClick: () => void;
+  deletable?: boolean;
+};
+
+const MultiSelectItem = ({ label, onRemoveClick, deletable = true }: Props) => {
   return (
     <div className="py border-nextadmin-border-default dark:border-dark-nextadmin-border-strong text-nextadmin-content-inverted dark:text-dark-nextadmin-content-inverted dark:hover:bg-dark-nextadmin-background-muted/50 hover:bg-nextadmin-background-muted flex cursor-pointer items-center justify-center rounded-md border px-2 text-sm">
       {label}
-      <button
-        type="button"
-        className="ml-1 flex-shrink-0 cursor-pointer text-gray-400 hover:text-gray-500"
-        onMouseDown={onRemoveClick}
-      >
-        <XMarkIcon className="h-4 w-4" />
-      </button>
+      {deletable && (
+        <button
+          type="button"
+          className="ml-1 flex-shrink-0 cursor-pointer text-gray-400 hover:text-gray-500"
+          onMouseDown={onRemoveClick}
+        >
+          <XMarkIcon className="h-4 w-4" />
+        </button>
+      )}
     </div>
   );
 };
