7026ad426d9b8d9cce5bc39b9531f3724fed6577
quentingrchr <q.grancher@gmail.com>
2024-08-01 14:20:58 +0200

    add OptionsTable in v5 docs

--- apps/docs/components/OptionsTable.tsx
+++ apps/docs/components/OptionsTable.tsx
@@ -1,20 +1,47 @@
 interface Option {
   name: string;
-  type: string;
+  type?: string;
   description: string | React.ReactNode;
   defaultValue?: string;
 }
 
-export function OptionsTable({ options }: { options: Option[] }) {
+interface HeadersLabel {
+  name: string;
+  type: string;
+  description: string;
+  defaultValue: string;
+}
+
+interface OptionsTableProps {
+  options: Option[];
+  labels?: HeadersLabel;
+}
+
+export function OptionsTable({ options, labels }: OptionsTableProps) {
+  const hasTypeColumn = options.some((option) => Boolean(option.type));
+  const hasDefaultValueColumn = options.some((option) =>
+    Boolean(option.defaultValue)
+  );
+
   return (
     <div className="-mx-6 mb-4 mt-6 overflow-x-auto overscroll-x-contain px-6 pb-4">
       <table className="w-full border-collapse text-sm">
         <thead>
           <tr className="border-b py-4 text-left dark:border-neutral-700">
-            <th className="py-2 font-semibold">Option</th>
-            <th className="py-2 pl-6 font-semibold">Type</th>
-            <th className="px-6 py-2 font-semibold">Description</th>
-            <th className="px-6 py-2 font-semibold">Default</th>
+            <th className="py-2 font-semibold">{labels?.name || "Name"}</th>
+            {hasTypeColumn && (
+              <th className="py-2 pl-6 font-semibold">
+                {labels?.type || "Type"}
+              </th>
+            )}
+            <th className="px-6 py-2 font-semibold">
+              {labels?.description || "Description"}
+            </th>
+            {hasDefaultValueColumn && (
+              <th className="px-6 py-2 font-semibold">
+                {labels?.defaultValue || "Default Value"}
+              </th>
+            )}
           </tr>
         </thead>
         <tbody className="align-baseline text-gray-900 dark:text-gray-100">
@@ -26,13 +53,17 @@ export function OptionsTable({ options }: { options: Option[] }) {
               <td className="whitespace-pre py-2 font-mono text-xs font-semibold leading-6 text-violet-600 dark:text-violet-500">
                 {name}
               </td>
-              <td className="whitespace-pre py-2 pl-6 font-mono text-xs font-semibold leading-6 text-slate-500 dark:text-slate-400">
-                {type}
-              </td>
+              {hasTypeColumn && (
+                <td className="whitespace-pre py-2 pl-6 font-mono text-xs font-semibold leading-6 text-slate-500 dark:text-slate-400">
+                  {Boolean(type) ? type : "-"}
+                </td>
+              )}
               <td className="py-2 pl-6">{description}</td>
-              <td className="py-2 pl-6 font-mono text-xs text-slate-500 dark:text-slate-400">
-                {Boolean(defaultValue) ? defaultValue : "-"}
-              </td>
+              {hasDefaultValueColumn && (
+                <td className="py-2 pl-6 font-mono text-xs text-slate-500 dark:text-slate-400">
+                  {Boolean(defaultValue) ? defaultValue : "-"}
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
