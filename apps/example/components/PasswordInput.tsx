526fd7e68a4e667d4bef64f6d9cf80c49403d469
Facundo Spira <facundospira@gmail.com>
2024-09-09 07:29:28 -0300

    fix: missing check to e2e

--- apps/example/components/PasswordInput.tsx
+++ apps/example/components/PasswordInput.tsx
@@ -21,7 +21,6 @@ const PasswordInput = (props: CustomInputProps) => {
         }}
         type="button"
         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        id="change-password"
       >
         {changePassword ? "Close" : "Change Password"}
       </button>
