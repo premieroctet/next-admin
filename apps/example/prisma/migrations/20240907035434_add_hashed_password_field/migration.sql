2354fca3d62ad5b2c9a4bb36629cd1b2d9736157
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2024-09-09 15:42:11 +0200

    update migration file

--- apps/example/prisma/migrations/20240907035434_add_hashed_password_field/migration.sql
+++ apps/example/prisma/migrations/20240907035434_add_hashed_password_field/migration.sql
@@ -1,8 +1,11 @@
 /*
-  Warnings:
-
-  - Added the required column `hashedPassword` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
+ Warnings:
+ 
+ - Added the required column `hashedPassword` to the `User` table without a default value. This is not possible if the table is not empty.
+ 
+ */
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "hashedPassword" TEXT NOT NULL;
+ALTER TABLE
+  "User"
+ADD
+  COLUMN "hashedPassword" TEXT;
