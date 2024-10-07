81b2e54b40b212dd9fdd7211e1d57bd6ced3c176
Regourd Colin <cregourd@premieroctet.com>
2024-10-07 12:04:11 +0200

    Nullable realtionship (#473)

--- packages/next-admin/src/components/inputs/SelectWidget.tsx
+++ packages/next-admin/src/components/inputs/SelectWidget.tsx
@@ -36,7 +36,7 @@ const SelectWidget = ({
 
   const { basePath } = useConfig();
 
-  const handleChange = (option: Enumeration) => {
+  const handleChange = (option: Enumeration | null) => {
     setFieldDirty(props.name);
     onChange(option);
     onClose();
@@ -95,7 +95,7 @@ const SelectWidget = ({
               className="flex items-center"
               onClick={(e) => {
                 e.preventDefault();
-                onChange({});
+                handleChange(null);
               }}
             >
               <XMarkIcon className="h-5 w-5 cursor-pointer text-gray-400" />
