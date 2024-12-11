e742cb058530052a5637abe21f4c62120d1e9243
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2024-12-11 17:38:09 +0100

    fix click outside

--- packages/next-admin/src/hooks/useCloseOnOutsideClick.ts
+++ packages/next-admin/src/hooks/useCloseOnOutsideClick.ts
@@ -12,7 +12,15 @@ const useClickOutside = <E extends HTMLElement = HTMLElement>(
       }
     };
 
-    document.addEventListener("click", handleClickOutside);
+    setTimeout(() => {
+      /**
+       * There is a weird behavior on Page Router where this triggers
+       * just when a selector is opened. I suppose this is because the click event is not finished
+       * dispatching when we click on a trigger, but this is strange that
+       * this does not occur in App Router
+       */
+      document.addEventListener("click", handleClickOutside);
+    });
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
