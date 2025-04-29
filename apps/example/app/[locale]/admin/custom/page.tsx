35a6d4d1ea0fcc043cd7e66f3ff5e3e1f786208b
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-04-29 18:12:49 +0200

    add missing adapter in custom pages

--- apps/example/app/[locale]/admin/custom/page.tsx
+++ apps/example/app/[locale]/admin/custom/page.tsx
@@ -1,4 +1,5 @@
 import { MainLayout } from "@premieroctet/next-admin";
+import { NextAdminRouterAdapter } from "@premieroctet/next-admin/adapters/next";
 import { getMainLayoutProps } from "@premieroctet/next-admin/appRouter";
 import { options } from "../../../../options";
 import { prisma } from "../../../../prisma";
@@ -21,40 +22,42 @@ const CustomPage = async () => {
   ];
 
   return (
-    <MainLayout
-      {...mainLayoutProps}
-      user={{
-        data: {
-          name: "John Doe",
-        },
-        logout: ["/"],
-      }}
-    >
-      <div className="p-10">
-        <h1 className="mb-4 text-xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-gray-300">
-          Dashboard
-        </h1>
-        <div className="mt-2">
-          <div>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-              {stats.map((item) => (
-                <div
-                  key={item.name}
-                  className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 dark:bg-gray-800"
-                >
-                  <dt className="truncate text-sm font-medium text-gray-500 dark:text-gray-400">
-                    {item.name}
-                  </dt>
-                  <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900 dark:text-gray-200">
-                    {item.stat}
-                  </dd>
-                </div>
-              ))}
-            </dl>
+    <NextAdminRouterAdapter>
+      <MainLayout
+        {...mainLayoutProps}
+        user={{
+          data: {
+            name: "John Doe",
+          },
+          logout: ["/"],
+        }}
+      >
+        <div className="p-10">
+          <h1 className="mb-4 text-xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-gray-300">
+            Dashboard
+          </h1>
+          <div className="mt-2">
+            <div>
+              <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                {stats.map((item) => (
+                  <div
+                    key={item.name}
+                    className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6 dark:bg-gray-800"
+                  >
+                    <dt className="truncate text-sm font-medium text-gray-500 dark:text-gray-400">
+                      {item.name}
+                    </dt>
+                    <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900 dark:text-gray-200">
+                      {item.stat}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
           </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </NextAdminRouterAdapter>
   );
 };
 
