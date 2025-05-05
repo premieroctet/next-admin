1c3361d13acca45f44c9714a9584ab926f3b1c50
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-05-05 11:03:08 +0200

    wip

--- apps/example-remix/vite.config.ts
+++ apps/example-remix/vite.config.ts
@@ -1,3 +1,5 @@
+import { createRequire } from "node:module";
+import path from "node:path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,6 +9,11 @@ declare module "@remix-run/node" {
     v3_singleFetch: true;
   }
 }
+
+const require = createRequire(import.meta.url);
+const prismaClientPath = require
+  .resolve("@prisma/client")
+  .replace(/@prisma(\/|\\)client(\/|\\).*/, ".prisma/client");
 
 export default defineConfig({
   plugins: [
@@ -24,5 +31,13 @@ export default defineConfig({
   ssr: {
     noExternal: ["react-datepicker"],
     external: ["@premieroctet/next-admin"],
+  },
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser": path.join(
+        prismaClientPath,
+        "index-browser.js"
+      ),
+    },
   },
 });
