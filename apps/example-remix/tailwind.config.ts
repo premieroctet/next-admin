f91f8c792ea29190c5b2fb3fbe49590456896ebd
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-05-16 16:13:43 +0200

    fix: tailwind config for remix & start

--- apps/example-remix/tailwind.config.ts
+++ apps/example-remix/tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   content: [
     "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@premieroctet/next-admin/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/examples-common/dist/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/examples-common/dist/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [nextAdminPreset],
 } satisfies Config;
--- apps/example-start/tailwind.config.js
+++ apps/example-start/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@premieroctet/next-admin/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/examples-common/dist/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/examples-common/dist/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [require("examples-common/preset")],
 };
