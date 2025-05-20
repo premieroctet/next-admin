ee7fda62fcb5cdc1681b8eac43b9a0d9955dbfb2
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-05-20 15:09:16 +0200

    fix build

--- packages/next-admin/rollup.config.ts
+++ packages/next-admin/rollup.config.ts
@@ -3,6 +3,14 @@ import { glob } from "glob";
 import { defineConfig } from "rollup";
 import preserveDirectives from "rollup-preserve-directives";
 import { copyFileSync } from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const basePath = path.dirname(fileURLToPath(import.meta.url));
+
+const themeCssPath = path.resolve(basePath, "src/theme.css");
+
+console.log("themeCssPath", themeCssPath);
 
 export default defineConfig({
   plugins: [
@@ -11,10 +19,15 @@ export default defineConfig({
     {
       name: "cp-theme-css",
       buildStart: function () {
-        this.addWatchFile("src/theme.css");
+        if (this.meta.watchMode) {
+          this.addWatchFile(themeCssPath);
+        }
       },
       buildEnd: function () {
-        copyFileSync("src/theme.css", "dist/theme.css");
+        // TODO: this only work in watch mode but not in build mode ?????
+        if (this.meta.watchMode) {
+          copyFileSync(themeCssPath, path.resolve(basePath, "dist/theme.css"));
+        }
       },
     },
   ],
