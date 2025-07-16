7198a54dbdbcdecccc8e45b0bd30deae9fae24c6
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-07-16 11:44:24 +0200

    set minify to false to keep things readable

--- packages/database/rslib.config.ts
+++ packages/database/rslib.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     minify: {
       jsOptions: {
         minimizerOptions: {
+          minify: false,
           format: {
             comments: "all",
           },
