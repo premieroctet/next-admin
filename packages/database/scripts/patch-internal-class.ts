9b328f2db8ba0a1e29e830e84d4c5c28578ad53e
Hugo FOYART <11079152+foyarash@users.noreply.github.com>
2025-07-17 15:55:50 +0200

    add vercel env var

--- packages/database/scripts/patch-internal-class.ts
+++ packages/database/scripts/patch-internal-class.ts
@@ -1,6 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
-import { fileURLToPath } from "url";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 
 interface Transformation {
   pattern: RegExp;
